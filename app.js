ed2bef8a837c39c1d12812875ebb21c7e0d65e8f
navyads <navyads22@gmail.com>
2022-03-28 09:57:14 +0000

    Shopee2 commit

---
+++ app.js
@@ -0,0 +1,7 @@
+let cnt=0;
+function count(){
+    //var btn=document.getElementById('productAdd1');
+    cnt+=1;
+    document.getElementById('span').innerHTML=cnt;
+    alert("Item added to the cart");
+}
